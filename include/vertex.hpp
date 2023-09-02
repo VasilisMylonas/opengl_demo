ffd0a975005a0cfc0b714ac0b599dcea46825146
Vasilis Mylonas <vasilismylonas@protonmail.com>
2023-09-02 12:27:44 +0300

    public vertex members

--- include/vertex.hpp
+++ include/vertex.hpp
@@ -1,12 +1,6 @@
 #pragma once
 
-class Vertex
+struct Vertex
 {
-public:
-    Vertex(float x, float y, float z) : x_{x}, y_{y}, z_{z}
-    {
-    }
-
-private:
-    float x_, y_, z_;
+    float x, y, z;
 };
