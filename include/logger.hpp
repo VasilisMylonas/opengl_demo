789c52b1b89e1836137b8639d7125c597800bbcc
Vasilis Mylonas <vasilismylonas@protonmail.com>
2023-09-10 20:47:11 +0300

    Logger improvements

--- include/logger.hpp
+++ include/logger.hpp
@@ -6,17 +6,25 @@
 
 class Logger
 {
-private:
-    std::string_view source_;
-    std::ostream& stream_;
-
 public:
-    Logger(std::ostream& stream, std::string_view source);
+    enum class Level
+    {
+        TRACE,
+        DEBUG,
+        INFO,
+        WARN,
+        ERROR,
+    };
+
+    Logger(std::ostream& stream, std::string_view source, Level level = Level::INFO);
     ~Logger();
     Logger(Logger&& other);
+    Logger& operator=(Logger&& other);
     Logger(const Logger&) = delete;
     Logger& operator=(const Logger&) = delete;
-    // TODO: move assignment
+
+    void set_level(Level level);
+    Level level() const;
 
     void trace(const char* format, ...) const;
     void debug(const char* format, ...) const;
@@ -24,5 +32,13 @@ public:
     void warn(const char* format, ...) const;
     void error(const char* format, ...) const;
 
-    void output(std::string_view level, const char* format, std::va_list args) const;
+protected:
+    static std::string_view level_to_str(Level level);
+
+    virtual void log(Level level, const char* format, std::va_list args) const;
+
+private:
+    std::string_view source_;
+    std::ostream* stream_;
+    Level level_;
 };
--- src/logger.cpp
+++ src/logger.cpp
@@ -1,48 +1,116 @@
 #include "logger.hpp"
 
+#include <cassert>
 #include <ctime>
 #include <iomanip>
 
-void Logger::output(std::string_view level, const char* format, std::va_list args) const
+std::string_view Logger::level_to_str(Level level)
 {
+    switch (level)
+    {
+    case Level::TRACE:
+        return "TRACE";
+    case Level::DEBUG:
+        return "DEBUG";
+    case Level::INFO:
+        return "INFO";
+    case Level::WARN:
+        return "WARN";
+    case Level::ERROR:
+        return "ERROR";
+    }
+
+    assert(false);
+}
+
+void Logger::log(Level level, const char* format, std::va_list args) const
+{
+    if (level < level_)
+    {
+        return;
+    }
+
     char* message;
     vasprintf(&message, format, args);
 
     std::time_t time = std::time(NULL);
     std::tm local = *std::localtime(&time);
 
-    stream_ << std::put_time(&local, "%H:%M:%S") << " [" << source_ << "] (" << level << ") "
-            << message << "\n";
+    *stream_ << std::put_time(&local, "%H:%M:%S") << " [" << source_ << "] (" << level_to_str(level)
+             << ") " << message << "\n";
 
     free(message);
 }
 
-#define LOG_FUNCTION(function)                                                                     \
-    void Logger::function(const char* format, ...) const                                           \
-    {                                                                                              \
-        std::va_list args;                                                                         \
-        va_start(args, format);                                                                    \
-        output(#function, format, args);                                                           \
-        va_end(args);                                                                              \
-    }
+void Logger::trace(const char* format, ...) const
+{
+    std::va_list args;
+    va_start(args, format);
+    log(Level::TRACE, format, args);
+    va_end(args);
+}
 
-LOG_FUNCTION(error)
-LOG_FUNCTION(warn)
-LOG_FUNCTION(info)
-LOG_FUNCTION(debug)
-LOG_FUNCTION(trace)
+void Logger::debug(const char* format, ...) const
+{
+    std::va_list args;
+    va_start(args, format);
+    log(Level::DEBUG, format, args);
+    va_end(args);
+}
 
-#undef LOG_FUNCTION
+void Logger::info(const char* format, ...) const
+{
+    std::va_list args;
+    va_start(args, format);
+    log(Level::INFO, format, args);
+    va_end(args);
+}
 
-Logger::Logger(std::ostream& stream, std::string_view source) : source_{source}, stream_{stream}
+void Logger::warn(const char* format, ...) const
+{
+    std::va_list args;
+    va_start(args, format);
+    log(Level::WARN, format, args);
+    va_end(args);
+}
+
+void Logger::error(const char* format, ...) const
+{
+    std::va_list args;
+    va_start(args, format);
+    log(Level::ERROR, format, args);
+    va_end(args);
+}
+
+Logger::Logger(std::ostream& stream, std::string_view source, Level level)
+    : source_{source}, stream_{&stream}, level_{level}
 {
 }
 
 Logger::~Logger()
 {
-    stream_ << std::flush;
+    *stream_ << std::flush;
 }
 
-Logger::Logger(Logger&& other) : source_{std::move(other.source_)}, stream_{other.stream_}
+Logger::Logger(Logger&& other)
+    : source_{std::move(other.source_)}, stream_{other.stream_}, level_{other.level_}
 {
+}
+
+Logger& Logger::operator=(Logger&& other)
+{
+    source_ = std::move(other.source_);
+    stream_ = other.stream_;
+    level_ = other.level_;
+    return *this;
+}
+
+void Logger::set_level(Level level)
+{
+    level_ = level;
+}
+
+Logger::Level Logger::level() const
+{
+    return level_;
 }
