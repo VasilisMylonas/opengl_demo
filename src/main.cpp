61ad259653404372c183b1f3fc91822c33a4e053
Vasilis Mylonas <vasilismylonas@protonmail.com>
2025-09-28 13:07:19 +0300

    Rework main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#include "gl/texture.hpp"
-#include "util.hpp"
 #include "window.hpp"
 
 #include <array>
@@ -8,7 +6,6 @@
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <glm/ext/vector_float3.hpp>
 #include <imgui.h>
 
 #include "gl/buffer.hpp"
@@ -17,6 +14,7 @@
 #include "gl/shader.hpp"
 #include "gl/vertex_array.hpp"
 #include "gl/vertex_layout.hpp"
+#include "util.hpp"
 
 void glfw_error([[maybe_unused]] int error, const char* description)
 {
@@ -33,16 +31,97 @@ void set_font(const char* font_path, float font_size)
     }
 }
 
-std::array<glm::vec3, 3> vertices = {
-    glm::vec3(-0.5f, -0.5f, 0.0f),
-    glm::vec3(0.5f, -0.5f, 0.0f),
-    glm::vec3(0.0f, 0.5f, 0.0f),
-};
+class my_app
+{
+public:
+    gl::vertex_array vao;
+    gl::program program;
+    gl::vertex_buffer<glm::vec3> vbo;
+    gl::index_buffer<unsigned int> ibo;
 
-std::array<unsigned int, 3> indices = {
-    0,
-    1,
-    2,
+    std::array<glm::vec3, 3> vertices = {
+        glm::vec3(-0.5f, -0.5f, 0.0f),
+        glm::vec3(0.5f, -0.5f, 0.0f),
+        glm::vec3(0.0f, 0.5f, 0.0f),
+    };
+
+    std::array<unsigned int, 3> indices = {
+        0,
+        1,
+        2,
+    };
+
+    bool close = false;
+
+    void load_data()
+    {
+        vbo.data(vertices.size(), vertices.data(), gl::buffer_usage::static_draw);
+        ibo.data(indices.size(), indices.data(), gl::buffer_usage::static_draw);
+    }
+
+    void load_shaders()
+    {
+        gl::shader vs(gl::shader_type::vertex);
+        gl::shader fs(gl::shader_type::fragment);
+
+        vs.set_source(read_file("./shaders/shader.vs"));
+        vs.compile();
+
+        fs.set_source(read_file("./shaders/shader.fs"));
+        fs.compile();
+
+        program.attach(vs);
+        program.attach(fs);
+        program.link();
+        program.use();
+    }
+
+    void init()
+    {
+        load_shaders();
+        load_data();
+
+        // gl::texture texture(0);
+        // texture.source_path("./image.jpg");
+
+        gl::vertex_layout layout;
+        layout.push_back({
+            .stride = sizeof(glm::vec3),
+            .offset = 0,
+            .type = gl::type_of<glm::vec3>(),
+        });
+
+        vao.buffers(vbo, ibo, layout);
+    }
+
+    void render()
+    {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        vao.bind();
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
+        vao.unbind();
+
+        if (ImGui::BeginMainMenuBar())
+        {
+            if (ImGui::BeginMenu("File"))
+            {
+                if (ImGui::MenuItem("Load Texture"))
+                {
+                    // TODO
+                }
+
+                if (ImGui::MenuItem("Exit"))
+                {
+                    close = true;
+                }
+                ImGui::EndMenu();
+            }
+
+            ImGui::EndMainMenuBar();
+        }
+    }
 };
 
 int main()
@@ -59,80 +138,20 @@ int main()
     {
         window window(1200, 800, "OpenGL Demo", 4, 0);
         window.swap_interval(1); // Enable vsync
-
         window.make_current();
-
         set_font("./fonts/Roboto-Regular.ttf", 18.0f);
-
-        gl::shader vs(gl::shader_type::vertex);
-        vs.set_source(read_file("./shaders/shader.vs"));
-        vs.compile();
-
-        gl::shader fs(gl::shader_type::fragment);
-        fs.set_source(read_file("./shaders/shader.fs"));
-        fs.compile();
-
-        gl::program program;
-        program.attach(vs);
-        program.attach(fs);
-        program.link();
-        program.use();
-
-        gl::texture texture(0);
-        texture.source_path("./image.jpg")
-
-            gl::vertex_buffer<glm::vec3>
-                vbo(vertices.size(), gl::buffer_usage::static_draw);
-        gl::index_buffer<unsigned int> ibo(indices.size(), gl::buffer_usage::static_draw);
-
-        ibo.data(indices.size(), indices.data());
-        vbo.data(vertices.size(), vertices.data());
-
-        gl::vertex_layout layout;
-        layout.push_back({
-            .stride = sizeof(glm::vec3),
-            .offset = 0,
-            .type = gl::type_of<glm::vec3>(),
-        });
-
-        gl::vertex_array vao;
-        vao.buffers(vbo, ibo, layout);
 
         window.show();
 
-        bool close = false;
+        my_app app;
+        app.init();
 
-        while (!window.should_close() && !close)
+        while (!window.should_close() && !app.close)
         {
             glfwPollEvents();
 
             window.begin_frame();
-
-            glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-            glClear(GL_COLOR_BUFFER_BIT);
-
-            vao.bind();
-            glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
-            vao.unbind();
-
-            if (ImGui::BeginMainMenuBar())
-            {
-                if (ImGui::BeginMenu("File"))
-                {
-                    if (ImGui::MenuItem("Exit"))
-                    {
-                        close = true;
-                    }
-                    ImGui::EndMenu();
-                }
-                if (ImGui::BeginMenu("Help"))
-                {
-                    ImGui::MenuItem("About");
-                    ImGui::EndMenu();
-                }
-                ImGui::EndMainMenuBar();
-            }
-
+            app.render();
             window.end_frame();
 
             window.swap_buffers();
